c45c217f3d04833d6c2c60f7ae8b8effeca2df16
Shalisia-Earth <shalisia-earth@ShalisiarthsMBP.home>
2024-05-04 16:08:05 -0400

    hello world

---
+++ HW_01_JS/index.js
@@ -0,0 +1,5 @@
+console.log ("Welcome to this magic eight ball application");
+const first_name = "Shalisia-Earth";
+const last_name = "Hyman";
+let question= "Will I ever be proficient in JS";
+console.log ("Hello my name is Shalisia-Earth Hyman and my question is: Will I ever become proficient in JS");
